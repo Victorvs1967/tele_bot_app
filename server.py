c0c2de6c7e49a8e336e2e7b55ed65edee07a1ec6
Victorvs1967 <victorsmirnov@me.com>
2019-12-14 17:51:29 +0200

    edit

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from zodiak import bot
 from flask import Flask
+import os
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def bot_start():
-    return 'Bot is runing....'
+    return 'Bot is runing....', 200
 
 @app.route('/run')
 def bot_run():
@@ -16,3 +17,5 @@ def bot_run():
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
+
+app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
