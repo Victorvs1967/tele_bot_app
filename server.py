545d82067bbde33649a04e57c2cfa7a327b382de
Victorvs1967 <victorsmirnov@me.com>
2019-12-15 13:29:41 +0200

    edit server

--- server.py
+++ server.py
@@ -8,19 +8,13 @@ flag = False
 
 @app.route('/')
 def bot_start():
-    return 'Bot is runing....', 200
-
-@app.route('/run')
-def bot_run():
     global flag
     if flag:
-        return 'Bot is runing....'
+        return 'Bot is runing....', 200
     bot.polling(none_stop=True, interval=0)
     flag = True
-    return 'Bot is starting....'
+    return 'Bot is starting....', 200
 
 
 if __name__ == "__main__":
-    pass
-# bot.polling(none_stop=True, interval=0)
-# app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
