ccd9a3660e73a189e9dbfa258c28004baa8dbef2
Victorvs1967 <victorsmirnov@me.com>
2019-12-15 16:07:13 +0200

    rermake bot

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+from config import TOKEN, HEROKU_APP_URL
 from zodiak import bot
-from flask import Flask
+from flask import Flask, request
 import os
 
 
@@ -8,16 +9,27 @@ flag = True
 
 @app.route('/')
 def bot_start():
-    return '<h2>Bot is runing...</h2><a href="/bot">Bot</a>', 200
+    return '<h2>Bot is runing...</h2>', 200
 
-@app.route('/bot')
-def bot_run():
-    global flag
-    if flag:
-        bot.polling()
-        # bot.polling(none_stop=True, interval=0)
-        flag = False
-    return 'Bot is starting....', 200
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=HEROKU_APP_URL + TOKEN)
+    return "!", 200
+
+# @app.route('/bot')
+# def bot_run():
+#     global flag
+#     if flag:
+#         bot.polling()
+#         # bot.polling(none_stop=True, interval=0)
+#         flag = False
+#     return 'Bot is starting....', 200
 
 
 if __name__ == "__main__":
