831f44b8177718a3c040d3f8f1e180ae194ae4ec
Victorvs1967 <victorsmirnov@me.com>
2019-12-15 16:38:42 +0200

    ok

--- server.py
+++ server.py
@@ -5,31 +5,17 @@ import os
 
 
 app = Flask(__name__)
-flag = True
-
-# @app.route('/')
-# def bot_start():
-#     return '<h2>Bot is runing...</h2>', 200
 
 @app.route('/' + TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+    return "ok!", 200
   
 @app.route('/', methods=["GET"])
 def index():
     bot.remove_webhook()
     bot.set_webhook(url=HEROKU_APP_URL + TOKEN)
-    return "Hello from Heroku!", 200
-
-# @app.route('/bot')
-# def bot_run():
-#     global flag
-#     if flag:
-#         bot.polling()
-#         # bot.polling(none_stop=True, interval=0)
-#         flag = False
-#     return 'Bot is starting....', 200
+    return '<h2>Runing zodiac bot...</h2>', 200
 
 
 if __name__ == "__main__":
